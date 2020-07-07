54abc3b30db6d1637d1cff2358fc931fb4fcc624
BrMrx <bruno@marx.cologne>
2020-07-07 19:06:23 +0200

    Bugfix

--- HPDevice.php
+++ HPDevice.php
@@ -737,7 +737,7 @@ abstract class HPDevice extends IPSModule {
    * HP_GetValue($id, $key)
    * Liefert einen Deviceparameter (siehe HP_SetValue)
    */
-  public function GetValue( integer $key) {
+  public function GetValue( $key) {
     switch ($key) {
       default:
         $value = GetValue(@IPS_GetObjectIDByIdent($key, $this->InstanceID));
